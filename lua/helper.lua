94b493298a17789e1d6fb16dd7543b6d0f66a265
mini <mini@minideMac-mini.local>
2024-05-26 08:18:32 +0800

    优化结构

--- lua/helper.lua
+++ lua/helper.lua
@@ -6,20 +6,22 @@ local function translator(input, seg)
   if input:find('^aid$') then
     local table = {
           { '拆分显隐', 'Ctrl + Shift + H' }
-        , { 'Emoji表情', 'Ctrl + Shift + M' }
-        , { '撤销上屏', 'Alt+Backspace' }
         , { '拼音显隐', 'Ctrl + Shift + J' }
         , { '字集切换', 'Ctrl + Shift + U' }
         , { '繁简切换', 'Ctrl + Shift + F' }
+        , { 'Emoji表情', 'Ctrl + Shift + M' }
         , { '临时拼音', 'z键引导临时拼音' }
         , { '重复历史', 'z键兼有重复历史' }
-        , { '选单', 'Ctrl+Shift+`' }
-        , { 'lua字符串', '以大写字母开头' }
-        , { '农历反查', '任意大写字母引导+数字日期' }
-        , { '时间', rv_var["date_var"] .. '｜' .. rv_var["time_var"] .. '｜' .. rv_var["week_var"] }
-        , { '历法', rv_var["nl_var"] .. '｜' .. rv_var["jq_var"] }
+        , { '方案选单', 'Ctrl+Shift+`' }
+        , { '大写数字', 'R(大写) + 数字' }
+        , { '公历转农历', 'N(大写) + 20240422' }
+        , { '农历', 'nl' }
+        , { '时间', 'time' }
+        , { '日期', 'date' }
+        , { '星期', 'week' }
         , { '全角', 'Shift + Space' }
-        , { '英标', 'Ctrl+ + .' }
+        , { '撤销上屏', 'Alt+Backspace' }
+        , { '英文标点', 'Ctrl+ + .' }
         , { '帮助', 'aid' }
         , { '注释', 'Ctrl + Shift + Return' }
     }
